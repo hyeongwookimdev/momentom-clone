c8ac470d7a5d2fc7b651ac525edae589bd48860b
Hyeongwoo Kim <hyeongwookim.dev@gmail.com>
2022-07-29 22:46:43 +0900

    Update style.css

--- js/background.js
+++ js/background.js
@@ -1,4 +1,4 @@
-/*const imagesLeft = [
+const images = [
   "0.jpg",
   "1.jpg",
   "2.jpg",
@@ -8,34 +8,13 @@
   "6.jpg",
   "7.jpg",
   "8.jpg",
-  "9.jpg",
-  "10.jpg",
-  "11.jpg",
-];
-const imagesRight = [
-  "0.jpg",
-  "1.jpg",
-  "2.jpg",
-  "3.jpg",
-  "4.jpg",
-  "5.jpg",
-  "6.jpg",
-  "7.jpg",
-  "8.jpg",
-  "9.jpg",
-  "10.jpg",
-  "11.jpg",
 ];
 
-const chosenImageLeft =
-  imagesLeft[Math.floor(Math.random() * imagesLeft.length)];
+const chosenImage = images[Math.floor(Math.random() * images.length)];
 const body = document.querySelector("body");
-body.style.background = `url(/img/img-left/${chosenImageLeft})`;
-*/
-/*
+
 const bgImage = document.createElement("img");
 
 bgImage.src = `img/${chosenImage}`;
 
 document.body.appendChild(bgImage);
-*/
