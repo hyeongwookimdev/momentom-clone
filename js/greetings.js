40d6f90cc06bc68afb8c461512907b7920ebf012
Hyeongwoo Kim <hyeongwookim.dev@gmail.com>
2022-07-30 20:14:28 +0900

    Add Log-In Screen

--- js/greetings.js
+++ js/greetings.js
@@ -1,6 +1,14 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
+const hello = document.querySelector(".main__greeting__hello");
+
+const header = document.querySelector(".header");
+const sideBar = document.querySelector(".side-bar");
+const mainTime = document.querySelector(".main__time");
+const coursesTitle = document.querySelectorAll(".main__courses__title");
+const courses = document.querySelectorAll(".main__courses");
+const footer = document.querySelector(".footer");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
@@ -10,9 +18,19 @@ const link = document.querySelector("a");
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
+  hello.classList.add(HIDDEN_CLASSNAME);
+  loginInput.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
   paintGreetings(username);
+  header.classList.remove(HIDDEN_CLASSNAME);
+  sideBar.classList.remove(HIDDEN_CLASSNAME);
+  mainTime.classList.remove(HIDDEN_CLASSNAME);
+  coursesTitle[0].classList.remove(HIDDEN_CLASSNAME);
+  coursesTitle[1].classList.remove(HIDDEN_CLASSNAME);
+  courses[0].classList.remove(HIDDEN_CLASSNAME);
+  courses[1].classList.remove(HIDDEN_CLASSNAME);
+  footer.classList.remove(HIDDEN_CLASSNAME);
 }
 
 function paintGreetings(username) {
@@ -24,7 +42,17 @@ const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if (savedUsername === null) {
   loginForm.classList.remove(HIDDEN_CLASSNAME);
+  hello.classList.remove(HIDDEN_CLASSNAME);
   loginForm.addEventListener("submit", onLoginSubmit);
 } else {
+  loginInput.classList.add(HIDDEN_CLASSNAME);
   paintGreetings(savedUsername);
+  header.classList.remove(HIDDEN_CLASSNAME);
+  sideBar.classList.remove(HIDDEN_CLASSNAME);
+  mainTime.classList.remove(HIDDEN_CLASSNAME);
+  coursesTitle[0].classList.remove(HIDDEN_CLASSNAME);
+  coursesTitle[1].classList.remove(HIDDEN_CLASSNAME);
+  courses[0].classList.remove(HIDDEN_CLASSNAME);
+  courses[1].classList.remove(HIDDEN_CLASSNAME);
+  footer.classList.remove(HIDDEN_CLASSNAME);
 }
